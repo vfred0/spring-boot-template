8712974c79ded3eb2d3e996ab8cf76794e7c9141
Víctor Arreaga <arreagav17@gmail.com>
2025-03-13 17:21:45 -0500

    chore: refactor

--- src/main/java/com/gnoboa/data/daos/IUserRoleRepository.java
+++
@@ -1,8 +0,0 @@
-//package com.gnoboa.data.daos;
-//
-//import com.gnoboa.data.entities.identity.UserRole;
-//import com.gnoboa.data.enums.Role;
-//
-//public interface IUserRoleRepository extends IRepository<UserRole, Integer> {
-//    UserRole findByName(Role name);
-//}
